c3e8c1f3347fae50bdba27b629e4a4c2ae10256e
Neel Joshi <nbjoshi@unc.edu>
2024-12-28 20:06:27 -0500

    Configured routes using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import "./App.css";
-
-function App() {
-  return <></>;
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+export default function App() {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 }
-
-export default App;
