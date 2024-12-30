5a34c10fc34e825ede7a675a3100270c2e2ded57
Neel Joshi <nbjoshi@unc.edu>
2024-12-29 23:09:06 -0500

    Added component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
 export default function App() {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className="grid grid-cols-1 gap-4 px-4 sm:px-6 lg:px-8">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
