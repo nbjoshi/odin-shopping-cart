6c8c1e07b2e9869bb50d55cfeb8bee58af85808e
Neel Joshi <nbjoshi@unc.edu>
2024-12-30 17:38:13 -0500

    Product card

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,23 @@
+import { ShopContext } from "../context/ShopContext";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+export default function ProductItem({ id, image, name, price }) {
+  const { currency } = useContext(ShopContext);
+  return (
+    <Link to={`product/${id}`} className="text-gray-700 cursor-pointer">
+      <div className="overflow-hidden">
+        <img
+          src={image}
+          alt={name}
+          className="hover:scale-110 transition ease-in-out"
+        />
+        <p className="pt-3 pb-1 text-sm">{name}</p>
+        <p className="text-sm font-medium">
+          {currency}
+          {price}
+        </p>
+      </div>
+    </Link>
+  );
+}
