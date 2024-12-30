21085b9c798a7f7b80b33f034ebdb86e2bab2de3
Neel Joshi <nbjoshi@unc.edu>
2024-12-29 23:11:12 -0500

    Almost finished Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import Policy from "../components/Policy";
+import NewsLetter from "../components/NewsLetter";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
+      <Policy />
+      <NewsLetter />
+    </div>
+  );
 }
