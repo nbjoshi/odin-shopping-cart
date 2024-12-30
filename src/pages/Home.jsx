cd0c0a51e39d3fc0b143ceecc109471692e441ee
Neel Joshi <nbjoshi@unc.edu>
2024-12-30 17:41:54 -0500

    Added NewArrivals component and fixed padding issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Hero from "../components/Hero";
+import NewArrivals from "../components/NewArrivals";
 import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
 import Policy from "../components/Policy";
@@ -8,10 +9,13 @@ export default function Home() {
   return (
     <div>
       <Hero />
+      <NewArrivals />
       <LatestCollection />
       <BestSeller />
-      <Policy />
-      <NewsLetter />
+      <div className="px-4 sm:px-6 lg:px-8">
+        <Policy />
+        <NewsLetter />
+      </div>
     </div>
   );
 }
