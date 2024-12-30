4e21a01e674651d5c37f87163c2ca8f3dad09a59
Neel Joshi <nbjoshi@unc.edu>
2024-12-30 17:41:03 -0500

    Fixed styling issue with grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+import PromoBanner from "./components/PromoBanner.jsx";
 
 export default function App() {
   return (
-    <div className="grid grid-cols-1 gap-4 px-4 sm:px-6 lg:px-8">
+    <div className="grid grid-cols-1 gap-4">
+      <PromoBanner />
       <Navbar />
       <Outlet />
       <Footer />
